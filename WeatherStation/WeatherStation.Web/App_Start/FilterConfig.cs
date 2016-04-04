b8722b18e892e03960effb737d6a78a62cb20248
AndreyBogach <boga4.andrey@gmail.com>
2016-04-04 15:50:06 +0300

    Added new project.

---
+++ WeatherStation/WeatherStation.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WeatherStation.Web
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
